ffa483b54fb9dde95820e32dd1e84115663e1f95
yangguoqing <ysh1059@163.com>
2020-02-13 23:45:43 +0800

    更新组件操作

--- demo01/src/index.js
+++ demo01/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Component_1 from './Component_1';
 
 /**
  * 自定义组件必须以大写字母开头
  */
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Component_1 />, document.getElementById('root'));
