7cbf965afff1f9f98c7ac27a3cd5a7411f4c14d0
yangguoqing <ysh1059@163.com>
2020-02-13 23:19:09 +0800

    组件化构造hello world

---
+++ demo01/src/App.js
@@ -0,0 +1,17 @@
+// 解构赋值
+import React, { Component } from 'react';
+/**
+ * 相当于
+ * import React from 'react';
+ * const Component = React.Component;
+ */
+
+class App extends Component {
+    render() {
+        return (
+            <div>Hello World!</div>
+        )
+    }
+}
+
+export default App;
