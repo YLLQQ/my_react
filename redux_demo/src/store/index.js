798c3c62cbeeef5663c2c58d2ce64d142b9db286
yangguoqing <ysh1059@163.com>
2020-02-14 20:52:21 +0800

    redux_dev_tools安装工具使用

--- redux_demo/src/store/index.js
+++ redux_demo/src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'redux';
 import reducer from './reducer'
 
-const store = createStore(reducer)
+const store = createStore(
+    reducer,
+    /* 如果windows有__REDUX_DEVTOOLS_EXTENSION__，则调用 */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store;
 
