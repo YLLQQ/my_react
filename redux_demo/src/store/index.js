076b922448aa28b11f4ed8a7fd20fe8a06293e77
yangguoqing <ysh1059@163.com>
2020-02-14 22:39:45 +0800

    redux三个注意事项

--- redux_demo/src/store/index.js
+++ redux_demo/src/store/index.js
@@ -1,6 +1,14 @@
 import { createStore } from 'redux';
 import reducer from './reducer'
 
+/**
+ * store必须唯一，多个store不允许，只能有一个store空间
+ * 只有store能改变自己的内容，reducer不能改变
+ * reducer必须是纯函数(
+ * 如果函数的调用参数相同，则永远返回相同结果。
+ * 它不依赖于程序执行期间函数外部任何状态或参数变化，必须只依赖于输入参数。
+ * )
+ */
 const store = createStore(
     reducer,
     /* 如果windows有__REDUX_DEVTOOLS_EXTENSION__，则调用 */
