27dbc045c189b5589488a8bf65fce1b8bbca3920
yangguoqing <ysh1059@163.com>
2020-02-14 15:57:15 +0800

    创建redux仓库中stroe和reducer, 并将数据进行绑定

--- redux_demo/src/TodoList.js
+++ redux_demo/src/TodoList.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-const data = [
-    '数据一',
-    '数据二',
-    '数据三'
-]
+import store from './store/index';
 
 class TodoList extends Component {
+
+    constructor(props) {
+        super(props)
+
+        console.log(store.getState())
+
+        this.state = store.getState()
+    }
+
     render() {
         return (
             <div style={{ margin: '10px' }}>
                 <div>
                     <Input
-                        placeholder='Write Something'
+                        placeholder={this.state.inputValue}
                         style={{ width: '250px', marginRight: '10px' }}
                     />
                     <Button type="primary">增加</Button>
                 </div>
                 <div style={{ margin: '10px', width: '300px' }}>
                     <List
-                        dataSource={data}
+                        dataSource={this.state.list}
                         bordered
                         renderItem={item => (<List.Item>{item}</List.Item>)}
                     />
---
+++ redux_demo/src/store/reducer.js
@@ -0,0 +1,12 @@
+const defaultState = {
+    inputValue: '写点什么',
+    list: [
+        '数据一',
+        '数据二',
+        '数据三'
+    ]
+}
+
+export default (state = defaultState, action) => {
+    return state
+}
