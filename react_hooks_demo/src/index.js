a5d8289807533f8a39af016ccb00c8e34ff0f909
yangguoqing <ysh1059@163.com>
2020-02-15 22:06:24 +0800

    react-hooks项目，useContext实现父子组件传值

--- react_hooks_demo/src/index.js
+++ react_hooks_demo/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Example from './Example'
 import Example2 from './Example2'
 import Example3 from './Example3';
+import Example4 from './Example4';
 
 
-ReactDOM.render(<Example3 />, document.getElementById('root'));
+ReactDOM.render(<Example4 />, document.getElementById('root'));
