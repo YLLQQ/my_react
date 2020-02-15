72cf9b3dc721fdd004bd75f082a23f1514ad16e2
yangguoqing <ysh1059@163.com>
2020-02-15 19:38:41 +0800

    react-router-dom后台获取路由

--- react_demo_01/src/AppRouter.js
+++ react_demo_01/src/AppRouter.js
@@ -6,6 +6,12 @@ import Video from './pages/Video'
 import Workplace from './pages/Workplace'
 
 const AppRouter = () => {
+    let routeConfig = [
+        { path: '/', title: '博客首页', exact: true, component: Index },
+        { path: '/video/', title: '视频教程', exact: false, component: Video },
+        { path: '/workplace/', title: '职场技能', exact: false, component: Workplace }
+    ]
+
     return (
         <Router>
             <div className="mainDiv">
@@ -13,32 +19,20 @@ const AppRouter = () => {
                     <h3>一级导航</h3>
 
                     <ul>
-                        <li>
-                            <Link to="/">博客首页</Link>
-                        </li>
-                        <li>
-                            <Link to="/video/">视频教程</Link>
-                        </li>
-                        <li>
-                            <Link to="/workplace/">职场技能</Link>
-                        </li>
+                        {
+                            routeConfig.map((item, index) => {
+                                return (<li key={index}> <Link to={item.path}>{item.title}</Link> </li>)
+                            })
+                        }
                     </ul>
                 </div>
 
                 <div className="rightMain">
-                    <Route
-                        path="/"
-                        exact
-                        component={Index}
-                    />
-                    <Route
-                        path="/video/"
-                        component={Video}
-                    />
-                    <Route
-                        path="/workplace/"
-                        component={Workplace}
-                    />
+                    {
+                        routeConfig.map((item, index) => {
+                            return (<Route key={index} exact={item.exact} path={item.path} component={item.component} />)
+                        })
+                    }
                 </div>
             </div>
         </Router>
