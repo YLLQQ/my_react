9f66834d2f331e1abbe47d70e4797455467bfe14
yangguoqing <ysh1059@163.com>
2020-02-15 21:53:58 +0800

    react-hooks项目，useEffect实现ComponentWillUnmount生命周期函数

--- react_hooks_demo/src/Example3.js
+++ react_hooks_demo/src/Example3.js
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+function Index() {
+    useEffect(() => {
+        console.log('Index')
+
+        return () => {
+            console.log('index leave')
+        }
+    }, [])
+
+    return <h2>Index</h2>
+}
+
+function List() {
+    useEffect(() => {
+        console.log('List')
+    })
+
+    return <h2>List</h2>
+}
 
 function Example3() {
     const [count, setCount] = useState(0)
 
+    /**
+     * react-hooks没有生命周期函数，使用useEffect进行替换
+     * 
+     * ComponentWillUnmount
+     */
     useEffect(() => {
         console.log(`useEffect => you clicked ${count} times`)
     })
@@ -11,6 +37,24 @@ function Example3() {
         <div>
             <p>You clicked {count} times</p>
             <button onClick={() => { setCount(count + 1) }}>click me</button>
+
+            <Router>
+                <ul>
+                    <li><Link to="/">Index</Link></li>
+                    <li><Link to="/list/">List</Link></li>
+                </ul>
+
+                <Route
+                    path="/"
+                    exact
+                    component={Index}
+                />
+                <Route
+                    path="/list/"
+                    component={List}
+                />
+            </Router>
+
         </div >
     );
 }
