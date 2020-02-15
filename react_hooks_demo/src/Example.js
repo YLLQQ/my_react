772a600e77b319122bda31876b6173c705524142
yangguoqing <ysh1059@163.com>
2020-02-15 21:05:06 +0800

    react-hooks项目，使用hooks方式替换原有组件方法

--- react_hooks_demo/src/Example.js
+++ react_hooks_demo/src/Example.js
@@ -1,24 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Exmple extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { count: 0 }
-    }
-    render() {
-        return (
-            <div>
-                <p>You clicked {this.state.count}</p>
-                <button onClick={this.addCount.bind(this)}>click me</button>
-            </div>
-        );
-    }
+function Example() {
+    const [count, setCount] = useState(0)
 
-    addCount() {
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => { setCount(count + 1) }}>click me</button>
+        </div >
+    );
 }
-
-export default Exmple;
+export default Example;
