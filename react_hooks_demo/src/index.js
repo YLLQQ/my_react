bbf128218d660b6fe5a10dcfb5ecc606d5672cdc
yangguoqing <ysh1059@163.com>
2020-02-15 20:59:47 +0800

    react-hooks项目初始化

--- react_hooks_demo/src/index.js
+++ react_hooks_demo/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Example from './Example'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Example />, document.getElementById('root'));
