4a2b21181a2865cff111f2845e8ec0c0453d2950
yangguoqing <ysh1059@163.com>
2020-02-15 17:17:15 +0800

    react-router精确匹配页面分离

--- react_demo/src/AppRouter.js
+++ react_demo/src/AppRouter.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-function Index() {
-    return <h2>JS-Index</h2>
-}
-
-function List() {
-    return <h2>JS-List</h2>
-}
+import Index from "./pages/Index";
+import List from "./pages/List";
 
 function AppRouter() {
     return (
         <Router>
             <ul>
                 <li><Link to="/">首页</Link></li>
-                <li><Link to="/list">列表</Link></li>
+                <li><Link to="/list/">列表</Link></li>
             </ul>
             <Route
                 path="/"
+                /*精确匹配 */
                 exact
                 component={Index}
             />
             <Route
                 path="/list/"
-                exact
                 component={List}
             />
 
