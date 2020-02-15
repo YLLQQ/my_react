27096e7635aa335fff31bfb55903e3f77020421c
yangguoqing <ysh1059@163.com>
2020-02-15 18:26:09 +0800

    react-router设置路由规则，并接受值

--- react_demo/src/pages/Index.js
+++ react_demo/src/pages/Index.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
+const list = [
+    { bid: 1, title: 'Book 1' },
+    { bid: 2, title: 'Book 2' },
+    { bid: 3, title: 'Book 3' },
+]
+
 
 const Index = () => {
     return (
         <div>
             <h2>JS-Index</h2>
-        </div>
+
+            <ul>
+                {
+                    list.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <Link to={'/list/' + item.bid}>
+                                    {item.bid} - {item.title}
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div >
     )
 }
 
